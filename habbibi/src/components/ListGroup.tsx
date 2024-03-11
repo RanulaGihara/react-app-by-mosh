223e69d92560fc643cfbd05b35297ef8f8fffbcc
ranulagihara <ranulaw@opusxenta.com>
2024-03-12 00:20:11 +0530

    condtional rendering can do like this

--- habbibi/src/components/ListGroup.tsx
+++ habbibi/src/components/ListGroup.tsx
@@ -2,12 +2,25 @@
 
 //pascal case - class name
 function ListGroup() {
-  const items = ["New ", "San ", "Colo", "Paris"];
+  let items = ["New ", "San ", "Colo", "Paris"];
+  items = [];
+
+  //conditional rendering but bellow is not a good practice
+
+  //   if (items.length === 0)
+  //     return (
+  //       <>
+  //         <h1>No items found</h1>
+  //       </>
+  //     );
 
   return (
     // this <> </> will tell react to use  fragment tag
     <>
       <h1>List Group</h1>
+      // we can use 2nd one instead of 1st
+      {items.length === 0 ? <p>no items there</p> : null}
+      {items.length === 0 && <p>no items there</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}> {item} </li>
