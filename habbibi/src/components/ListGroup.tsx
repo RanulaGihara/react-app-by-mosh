080bdd07656c2fb2c35a5c7b8134e4da86eae563
ranulagihara <ranulaw@opusxenta.com>
2024-03-13 22:15:28 +0530

    props

--- habbibi/src/components/ListGroup.tsx
+++ habbibi/src/components/ListGroup.tsx
@@ -1,40 +1,17 @@
-// import { Fragment } from "react";
-import { useState } from "react"; // build in function in react to idenftify this  component has data or state that can change by time
+import { useState } from "react";
 
-//pascal case - class name
-function ListGroup() {
-  let items = ["New ", "San ", "Colo", "Paris"];
-  //   //   items = [];
-  //   let selectedIndex = 0;
+interface ListGroupProps {
+  items: String[];
+  heading: String;
+//   onSelectItem: (items: string) => void;  //type of this op
+}
 
-  //Hook
+function ListGroup({ items, heading }: ListGroupProps) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
-  //   arr[0]  //variable selectedIndex
-  //   arr[1]  // updater
-
-  // here in TS we can get not only type safety also the suggestions, Also inside the return it identify the type. but here I have understand the type
-  //   const handleClick = (event: MouseEvent) => {
-  //     console.log(event);
-  //   };
-
-  //conditional rendering but bellow is not a good practice
-
-  //   if (items.length === 0)
-  //     return (
-  //       <>
-  //         <h1>No items found</h1>
-  //       </>
-  //     );
-
   return (
-    // this <> </> will tell react to use  fragment tag
-
-    // we can use 2nd one instead of 1st
-    //  {items.length === 0 ? <p>no items there</p> : null}
-
     <>
-      <h1>List Group</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>no items there</p>}
       <ul className="list-group">
         {items.map((item, index) => (
@@ -44,16 +21,8 @@ function ListGroup() {
                 ? "list-group-item active"
                 : "list-group-item "
             }
-            key={item}
+            key={index}
             onClick={() => setSelectedIndex(index)}
-
-            // onClick={}
-
-            // onClick={handleClick}
-
-            // onClick={() => {
-            //     console.log(item, index);
-            //   }}
           >
             {item}
           </li>
